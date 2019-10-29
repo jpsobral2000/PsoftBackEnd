a31326e4a36c793ae138b8c1bf87e26efd1f1ee7
Joao Sobral <joao.sobral@ccc.ufcg.edu.br>
2019-10-29 16:04:34 -0300

    JWT comecado

--- ajude/src/main/java/psoft/ufcg/ajude/Services/JWTService.java
+++ ajude/src/main/java/psoft/ufcg/ajude/Services/JWTService.java
@@ -1,4 +1,28 @@
 package psoft.ufcg.ajude.Services;
 
+import org.springframework.stereotype.Service;
+
+import javax.servlet.ServletException;
+
+@Service
 public class JWTService {
+
+    private  UsuarioService usuarioService;
+    private final String TOKEN_KEY = "teste";
+
+    public JWTService (UsuarioService usuarioService) {
+        super();
+        this.usuarioService = usuarioService;
+
+    }
+
+    public boolean existeUsuario (String autorizaztionHeader) throws ServletException {
+        String usuario = getAutorirazacao(autorizaztionHeader);
+        return false;
+    }
+
+    private String getAutorirazacao(String autorizaztionHeader) throws  ServletException{
+        return "";
+    }
+
 }
