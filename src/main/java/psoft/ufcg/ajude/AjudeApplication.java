6c1b998e572bd54a6838eaa3ea50a95c65e0404c
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2019-11-04 17:55:04 -0300

    Ajeitar deploy

--- src/main/java/psoft/ufcg/ajude/AjudeApplication.java
+++ src/main/java/psoft/ufcg/ajude/AjudeApplication.java
@@ -1,7 +1,9 @@
 package psoft.ufcg.ajude;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import psoft.ufcg.ajude.Filter.TokenFilter;
@@ -9,17 +11,17 @@ import psoft.ufcg.ajude.Filter.TokenFilter;
 @SpringBootApplication
 public class AjudeApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(AjudeApplication.class, args);
+	}
+
+
 	@Bean
 	public FilterRegistrationBean<TokenFilter> filterJwt(){
 		FilterRegistrationBean<TokenFilter> filterRB = new FilterRegistrationBean<TokenFilter>();
 		filterRB.setFilter(new TokenFilter());
 		filterRB.addUrlPatterns("/campanha/cadastro", "");
 		return filterRB;
-	}
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(AjudeApplication.class, args);
 	}
 
 }
